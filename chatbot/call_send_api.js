b7097bda3607b89915b7f58439a1739b732df7e8
Jieun Kim <wwooww374@gmail.com>
2018-08-10 18:03:12 +0900

    require request

--- chatbot/call_send_api.js
+++ chatbot/call_send_api.js
@@ -1,3 +1,5 @@
+const request = require('request');
+
 function callSendAPI(sender_psid, response) {
     // Construct the message body
     let request_body = {
@@ -10,7 +12,6 @@ function callSendAPI(sender_psid, response) {
     // Send the HTTP request to the Messenger Platform
     request({
       "uri": "https://graph.facebook.com/v2.6/me/messages",
-      // 원래는 환경변수 설정
       "qs": { "access_token": "EAAgHaJ6wjtMBAJ4XT7yxOvwJA8sv0azB2CC5RnqTcwfZC4trd9d5hx8htJKgcr32Jo0wSarXYUQtC9d70TKgLWZAbLy6Fhq9EnnhAdcU5fxqvIlEgVLkZAcHH3X8nlcZBChvG7S4wD1Aayb5mNgOEpipsqKEVxfUnZCMdDUbu31uTl64nCpTe" },
       "method": "POST",
       "json": request_body
